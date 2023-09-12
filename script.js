a82cf3e0b1b24624c49e58b5794497c129d21b0d
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-09-12 19:56:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function decimalToBinary(num) {
   //Write you code here
-	let bin=0;
-	let rem, i=1, step=1;
-	while (x != 0) {
-        rem = x % 2;
-        console.log(`Step ${step++}: ${x}/2, Remainder = ${rem}, Quotient = ${parseInt(x/2)}`
-        );
-        x = parseInt(x / 2);
+    let bin = 0;
+    let rem, i = 1, step = 1;
+    while (num != 0) {
+        rem = num % 2;
+        console.log(`Step ${step++}: ${num}/2, Remainder = ${rem}, Quotient = ${parseInt(num / 2)}`);
+        num = parseInt(num / 2);
         bin = bin + rem * i;
         i = i * 10;
     }
     console.log(`Binary: ${bin}`);
-  
 }
 
 window.decimalToBinary = decimalToBinary;
